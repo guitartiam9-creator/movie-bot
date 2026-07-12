28e96fa5e84b7cca789afb0df71a269d33a600dc
guitartiam9-creator <guitartiam9@gmail.com>
2026-07-12 03:51:37 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = http://t.me/HorrorVaultBot
-
+TOKEN = 8918158673:AAGzOK9NCgoiftO7XVWen3a2xi64cnPhfOk
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("سلام! ربات فیلم آماده است 🎬")
 
