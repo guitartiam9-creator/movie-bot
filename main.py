6aa6ddef473f24f1306d3c76ff5343ce3feac9ee
guitartiam9-creator <guitartiam9@gmail.com>
2026-07-12 04:08:44 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = "8918158673:AAH1zrfNMg5rfwqWjSLLm15s_7CBUIXO3Y8"
+TOKEN = "8918158673:AAEtODhVr06C0jkeH3KtDTYj9vKPwrVa8k0"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("سلام! ربات فیلم آماده است 🎬")
